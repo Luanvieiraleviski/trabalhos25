73d04a7ff2edf04e7b9e6e4266e4d891796666df
Luanvieiraleviski <luan.leviski@escola.pr.gov.br>
2025-03-10 08:24:22 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,19 @@
+const peso =60;
+const agua_ml = peso * 35;
+const agua_L = agua_ml / 1000; 
+console.log ("a quantidade de agua ideal e" + agua_L + "litros");
+
+
+
+
+
+const av1 = 10.0
+const av2 = 7.0
+const av3 = 4.0
+const media = (av1 + av2 + av3) / 3
+if (media>= 6.0) {
+    console.log("apovado")
+}else{
+    console.log("reprovado")
+}
+console.log("media de " + media)
