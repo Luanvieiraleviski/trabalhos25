be1a7ca26dec36bcd4621af0f54ac8d0184d5b1d
Luanvieiraleviski <luan.leviski@escola.pr.gov.br>
2025-03-11 11:54:54 -0300

    Add files via upload

--- lacorepeticao.js
+++ lacorepeticao.js
@@ -6,4 +6,13 @@ while (contador < -1){
 
 contador = contador + 1
 
+const precos = [12, 15, 15, 20, 12];
+const lanches = ["x-egg", "x-ovo", "x-bacon","x-tudo","x-frango"]
+const tamanho  = lanches.length
+let contador = 0;
+
+while (contador < tamanho)
+     console.log (lanches[contador] + " - R$ " + precos [contador]);
+contador = contador + 1
+
 }
