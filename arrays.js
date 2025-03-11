3fb1d982af49426934c80bcea7e74be7c5c3e540
Luanvieiraleviski <luan.leviski@escola.pr.gov.br>
2025-03-11 11:43:04 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,18 @@
+
+const lanches = ["x-egg", "x-ovo", "x-bacon","x-tudo","x-frango"]
+
+
+const precos = [12, 15, 15, 20, 12];
+
+
+console.log(lanches);
+console.log(precos);
+
+console.log(lanches[2] + " - R$ " + precos[0])
+console.log(lanches[3] + " - R$ " + precos[3])
+console.log(lanches[1] + " - R$ " + precos[1])
+console.log(lanches[4] + " - R$ " + precos[4])
+
+//emncontrar tamanho da lista
+const tamanho  = lanches.length
+console.log(tamanho);
---
+++ lacorepeticao.js
@@ -0,0 +1,9 @@
+let numero = 1
+while(numero < 25)
+     console.log(numero);
+numero = numero +25; 
+while (contador < -1){
+
+contador = contador + 1
+
+}
